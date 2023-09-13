9755456a153d441f3feb7447862c202c804c9c2a
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-09-13 12:05:32 -0300

    Feat: header construction

--- DogCare/src/app/header/header.component.ts
+++ DogCare/src/app/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit() {
+    const hamburger = document.getElementById('hamburger') as HTMLElement;
+    const itemsMenu = document.querySelector('.header-nav ul') as HTMLElement;
+
+    hamburger.addEventListener('click', () => {
+      itemsMenu.classList.toggle('show');
+    });
+  }
 }
